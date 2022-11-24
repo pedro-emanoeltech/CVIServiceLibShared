ee44b029e0082809f7547346b76faa45df5d25d9
Pedro Emanoel <pedro.emanoeltech@hotmail.com>
2022-11-24 02:44:10 -0300

    Ajustes Token

--- CVIServiceLibShared/Constants/Resource.cs
+++ CVIServiceLibShared/Constants/Resource.cs
@@ -9,25 +9,25 @@ namespace CVIServiceLibShared.Constants
     public class Resource
     { 
 
-       public const string  CANDIDATURA= "candidatura ";
+       public const string  CANDIDATURA= "candidatura";
        public const string  CARGO= "cargo";
        public const string  CIDADE = "cidade";
-       public const string  CONTA  = "conta ";
-       public const string  CONTATO  = "contato ";
+       public const string  CONTA  = "conta";
+       public const string  CONTATO  = "contato";
        public const string  CURSO  = "curso";
-       public const string  CURSOFORMACAOACADEMICA   = "cursoformacaoacademica ";
+       public const string  CURSOFORMACAOACADEMICA   = "cursoformacaoacademica";
        public const string  ENDERECO   = "endereco";
        public const string  ESCOLARIDADE   = "escolaridade";
        public const string  ESTADO   = "estado";
-       public const string  HISTORICOPROFISSIONAL   = "historicoprofissional  ";
-       public const string  IDIOMA  = "idioma ";
-       public const string  NACIONALIDADE  = "nacionalidade ";
-       public const string  OBJETIVO  = "objetivo ";
-       public const string  PAIS  = "pais ";
+       public const string  HISTORICOPROFISSIONAL   = "historicoprofissional";
+       public const string  IDIOMA  = "idioma";
+       public const string  NACIONALIDADE  = "nacionalidade";
+       public const string  OBJETIVO  = "objetivo";
+       public const string  PAIS  = "pais";
        public const string  PERFIL = "perfiL";
        public const string  SEGMENTO = "segmento";
-       public const string  VAGA = "vaga  ";
-       public const string  LOGIN = "login  ";
-       public const string  TOKEN = "token  ";
+       public const string  VAGA = "vaga";
+       public const string  LOGIN = "login";
+       public const string  TOKEN = "token";
     }
 }
