4748fbcd5c52901602338df7a936fcbe7a42481a
Pedro Emanoel <pedro.emanoeltech@hotmail.com>
2022-11-26 22:45:20 -0300

    add cadastro conta

---
+++ CVIServiceLibShared/Constants/Textos/Termos.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CVIServiceLibShared.Constants.Textos
+{
+    public  class Termos
+    {
+        public const string TERMOSDEUSO = "" +
+            "Licença MIT\r\n\r\nDireitos autorais (c) 2021 MudBlazor\r\n\r\nA permissão é concedida, gratuitamente, a qualquer pessoa que obtenha uma cópia\r\ndeste software e arquivos de documentação associados (o \"Software\"), para lidar\r\nno Software sem restrições, incluindo, sem limitação, os direitos\r\nusar, copiar, modificar, fundir, publicar, distribuir, sublicenciar e/ou vender\r\ncópias do Software e para permitir que as pessoas a quem o Software é\r\nmunidos para o efeito, nas seguintes condições:\r\n\r\nO aviso de direitos autorais acima e este aviso de permissão devem ser incluídos em todos os\r\ncópias ou partes substanciais do Software.\r\n\r\nO SOFTWARE É FORNECIDO \"COMO ESTÁ\", SEM GARANTIA DE QUALQUER TIPO, EXPRESSA OU\r\nIMPLÍCITAS, INCLUINDO, SEM LIMITAÇÃO, AS GARANTIAS DE COMERCIALIZAÇÃO,\r\nADEQUAÇÃO PARA UM FIM ESPECÍFICO E NÃO VIOLAÇÃO. EM NENHUM CASO O\r\nOS AUTORES OU DETENTORES DOS DIREITOS AUTORAIS SERÃO RESPONSÁVEIS POR QUALQUER REIVINDICAÇÃO, DANOS OU OUTROS\r\nRESPONSABILIDADE, SEJA EM UMA AÇÃO DE CONTRATO, ILÍCITO OU DE OUTRA FORMA, DECORRENTE DE,\r\nFORA DE OU EM CONEXÃO COM O SOFTWARE OU O USO OU OUTROS NEGÓCIOS NO\r\nPROGRAMAS.";
+
+        public const string TERMOLGPD = "Licença MIT\r\n\r\nDireitos autorais";
+    }
+}
